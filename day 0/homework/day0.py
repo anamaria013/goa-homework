31c6470c1ba79d6300c2da6bc9e29cdecd06129b
anamaria013 <tpuriateo11@gmail.com>
2024-10-15 23:21:55 +0400

    first commit

---
+++ day 0/homework/day0.py
@@ -0,0 +1,69 @@
+from turtle  import *
+
+
+
+
+
+
+begin_fill()
+color("purple")
+width(7)
+forward (200)
+left(90)
+
+
+
+forward(200)
+left (90)
+
+forward (200)
+left (90)
+
+forward (200)
+left (90)
+
+
+forward(70)
+
+left(90)
+end_fill()
+begin_fill()
+color("yellow")
+
+forward(120)
+right(90)
+
+forward(70)
+right(90)
+forward(120)
+end_fill()
+penup()
+goto(200,200)
+pendown()
+left(208)
+begin_fill()
+color("red")
+forward(200)
+left(121)
+
+forward(200)
+end_fill()
+
+penup()
+goto(50,50)
+pendown()
+
+begin_fill()
+color("blue")
+right(150)
+forward(40)
+left(90)
+forward(40)
+left(90)
+
+forward(40)
+left(90)
+
+forward(31)
+end_fill()
+exitonclick()
