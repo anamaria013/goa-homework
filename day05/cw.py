186e785432408e36ceef220a5ea084d22078c393
anamaria013 <tpuriateo11@gmail.com>
2024-11-25 13:23:13 +0400

    commit

--- day05/cw.py
+++
@@ -1,4 +0,0 @@
-#input არის ინფორმაციას რომ მიცემს utput არის გამომტანელი save as ინახავს  და როცა გინდა გამოაქვს
-name =input("enter your name:  ")
-surname = input("enter your surname:  " )
-print(surname + name)
---
+++ day08/hw/day08hw.py
@@ -0,0 +1,22 @@
+age = int((input("enter your age :  ")))
+age = float((input("enter your age :  ")))
+age = str((input("enter your age :  ")))
+age = True
+
+
+num = 10
+print(type(num))
+
+
+age = 5
+age = 10
+age = 15
+age = 20
+age = 25
+print(age+5)
+
+name= "anamaria"
+surname = "machaidze"
+age =10
+favorite_color = "pinke"
+print(name,surname, age ,favorite_color)
