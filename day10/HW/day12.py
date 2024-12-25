3add1f7301f311d4cc4864c965bbecca0774ccbb
anamaria013 <tpuriateo11@gmail.com>
2024-12-25 12:47:47 +0400

    commit

---
+++ day10/HW/day12.py
@@ -0,0 +1,10 @@
+a = 5 == 5 #true
+b = 10 != 20 #
+c = 4 > 3 and 2 < 5 #true
+d = 10 > 20 or 5 < 8  #true
+e = not (7 > 3) #false
+f = "hello" == "world"#
+g = 15 >= 10
+h = 8 <= 8
+i = 3 > 1 and 2 == 2 and 4 != 5
+j = not (10 < 5 or 2 == 2)
